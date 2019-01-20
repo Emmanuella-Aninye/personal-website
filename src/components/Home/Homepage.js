801bad7ac6967d3637feea631974ec6b5e4e7043
EMMANUELLA ANINYE <emmanuellaaninye@EMMANUELLAs-MBP.fios-router.home>
2019-01-19 21:55:11 -0500

    Changed the logo

--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -1,83 +1,85 @@
-import React , {Component} from 'react';
-import ReactFullpage from '@fullpage/react-fullpage';
+import React, { Component } from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
 import "./homepage.css";
 import "./override.css";
-class Homepage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          sectionsColor: [...originalColors],
-          fullpages: [
-            {
-              text: 'Welcome to my website. My name is E and I am a developer',
-              }
-          ],
-        };
-      }
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-      onLeave(origin, destination, direction) {
-        console.log('onLeave', { origin, destination, direction });
-        // arguments are mapped in order of fullpage.js callback arguments do something
-        // with the event
-      }
-    render() {
-        const { fullpages } = this.state;
-    
-        if (!fullpages.length) {
-          return null;
+class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sectionsColor: [...originalColors],
+      fullpages: [
+        {
+          text: "Welcome to my website. My name is E and I am a developer"
         }
-    
-        const Menu = () => (
-          <div
-            className="menu"
-            style={{
-              position: 'fixed',
-              top: 0,
-              zIndex: 100,
-              width: '100%'
-            }}
-          >
-          <ul>
-              <h3 onClick={console.log('SUP')}>
-                  Home
-              </h3>
-          </ul>
-          </div>
-        );
-    
-        return (
-          <div className="App">
-            <Menu />
-            <ReactFullpage
-              debug /* Debug logging */
-              scrollOverflow
-              navigation
-              onLeave={this.onLeave.bind(this)}
-              sectionsColor={this.state.sectionsColor}
-              pluginWrapper={pluginWrapper}
-              render={comp => (
-                <ReactFullpage.Wrapper>
-                  {fullpages.map(({ text }) => (
-                    <div key={text} className="section">
-                      <h2>{text}</h2>
-                    </div>
-                  ))}
-                </ReactFullpage.Wrapper>
-              )}
-            />
-          </div>
-        );
-      }
+      ]
+    };
+  }
+
+  onLeave(origin, destination, direction) {
+    console.log("onLeave", { origin, destination, direction });
+    // arguments are mapped in order of fullpage.js callback arguments do something
+    // with the event
+  }
+  render() {
+    const { fullpages } = this.state;
+
+    if (!fullpages.length) {
+      return null;
     }
 
+    const Header = () => (
+      <div
+        className="body"
+        style={{
+          position: "fixed",
+          top: 0,
+          zIndex: 100,
+          width: "100%"
+        }}
+      >
+        <Router>
+          <ul>
+            <h3>
+              <Link to="/home">Home</Link>
+            </h3>
+          </ul>
+        </Router>
+      </div>
+    );
+
+    return (
+      <div className="App">
+        <Header />
+        <ReactFullpage
+          debug /* Debug logging */
+          scrollOverflow
+          navigation
+          onLeave={this.onLeave.bind(this)}
+          sectionsColor={this.state.sectionsColor}
+          pluginWrapper={pluginWrapper}
+          render={comp => (
+            <ReactFullpage.Wrapper>
+              {fullpages.map(({ text }) => (
+                <div key={text} className="section">
+                  <h2>{text}</h2>
+                </div>
+              ))}
+            </ReactFullpage.Wrapper>
+          )}
+        />
+      </div>
+    );
+  }
+}
 
 const pluginWrapper = () => {
-    /**
-     * require('fullpage.js/vendors/scrolloverflow'); // Optional. When using scrollOverflow:true
-     */
-  };
-  
-  const originalColors = ['#001a33','#001a33','#001a33'];
-  
-    
+  /**
+   * require('fullpage.js/vendors/scrolloverflow'); // Optional. When using scrollOverflow:true
+   */
+};
+
+const originalColors = ["#001a33", "#001a33", "#001a33"];
+
 export default Homepage;
